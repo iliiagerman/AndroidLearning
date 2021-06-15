7163a6571b618e53d7c0456fa6338c8deead0262
iliia Germaniuk <iliia.germaniuk@gmail.com>
2021-06-15 20:41:30 +0300

    intent, creation two Activity...result -->now two screens can switch

--- app/src/main/java/com/german/androidlearning/MainActivity.java
+++ app/src/main/java/com/german/androidlearning/MainActivity.java
@@ -2,6 +2,7 @@ package com.german.androidlearning;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.Menu;
@@ -12,113 +13,27 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    final int MENU_RESET_ID = 1;
-    final int MENU_QUIT_ID = 2;
-    EditText etNum1;
-    EditText etNum2;
 
-    Button btnAdd;
-    Button btnSub;
-    Button btnMult;
-    Button btnDiv;
+    Button btnActTwo;
 
-    TextView tvResult;
-
-    ;
-    String oper = "";
-
-    /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-        // находим элементы
-        etNum1 = findViewById(R.id.etNum1);
-        etNum2 = findViewById(R.id.etNum2);
-
-        btnAdd =  findViewById(R.id.btnAdd);
-        btnSub =findViewById(R.id.btnSub);
-        btnMult = findViewById(R.id.btnMult);
-        btnDiv = findViewById(R.id.btnDiv);
-
-        tvResult =  findViewById(R.id.tvResult);
-
-        // прописываем обработчик
-        btnAdd.setOnClickListener(this);
-        btnSub.setOnClickListener(this);
-        btnMult.setOnClickListener(this);
-        btnDiv.setOnClickListener(this);
+        btnActTwo = findViewById(R.id.btnActTwo);
+        btnActTwo.setOnClickListener(this);
 
     }
-
     @Override
     public void onClick(View v) {
-        // TODO Auto-generated method stub
-        float num1 = 0;
-        float num2 = 0;
-        float result = 0;
-
-        // Проверяем поля на пустоту
-        if (TextUtils.isEmpty(etNum1.getText().toString())
-                || TextUtils.isEmpty(etNum2.getText().toString())) {
-            return;
-        }
-
-        // читаем EditText и заполняем переменные числами
-        num1 = Float.parseFloat(etNum1.getText().toString());
-        num2 = Float.parseFloat(etNum2.getText().toString());
-
-        // определяем нажатую кнопку и выполняем соответствующую операцию
-        // в oper пишем операцию, потом будем использовать в выводе
         switch (v.getId()) {
-            case R.id.btnAdd:
-                oper = "+";
-                result = num1 + num2;
-                break;
-            case R.id.btnSub:
-                oper = "-";
-                result = num1 - num2;
-                break;
-            case R.id.btnMult:
-                oper = "*";
-                result = num1 * num2;
-                break;
-            case R.id.btnDiv:
-                oper = "/";
-                result = num1 / num2;
+            case R.id.btnActTwo:
+                Intent intent = new Intent(this, MainActivity2.class);
+                startActivity(intent);
                 break;
             default:
                 break;
         }
-
-        // формируем строку вывода
-        tvResult.setText(num1 + " " + oper + " " + num2 + " = " + result);
-    }
-    // создание меню
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        menu.add(0, MENU_RESET_ID, 0, "Reset");
-        menu.add(0, MENU_QUIT_ID, 0, "Quit");
-        return super.onCreateOptionsMenu(menu);
-    }
-
-    // обработка нажатий на пункты меню
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case MENU_RESET_ID:
-                // очищаем поля
-                etNum1.setText("");
-                etNum2.setText("");
-                tvResult.setText("");
-                break;
-            case MENU_QUIT_ID:
-                // выход из приложения
-                finish();
-                break;
-        }
-        return super.onOptionsItemSelected(item);
     }
 }
+
