251862450f6647ef8bb7558db682f61efca7bad7
iliia Germaniuk <iliia.germaniuk@gmail.com>
2021-06-18 17:44:12 +0300

    add new activity

--- app/src/main/java/com/german/androidlearning/package2/MyActivity2.java
+++ app/src/main/java/com/german/androidlearning/package2/MyActivity2.java
@@ -2,15 +2,33 @@ package com.german.androidlearning.package2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.german.androidlearning.R;
 
-public class MyActivity2 extends AppCompatActivity {
+public class MyActivity2 extends AppCompatActivity implements View.OnClickListener {
 
+    Button qqqq;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.item);
+        qqqq = findViewById(R.id.qqqq);
+        qqqq.setOnClickListener((View.OnClickListener) this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.qqqq:
+                Intent intent3 = new Intent(this, MyActivity3.class);
+                startActivity(intent3);
+                break;
+            default:
+                break;
+        }
     }
 }
---
+++ app/src/main/java/com/german/androidlearning/package2/MyActivity3.java
@@ -0,0 +1,16 @@
+package com.german.androidlearning.package2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.german.androidlearning.R;
+
+public class MyActivity3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my3);
+    }
+}
