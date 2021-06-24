ad1bce8c4ff916dde1736768d98f88c82ed76dcd
iliia Germaniuk <iliia.germaniuk@gmail.com>
2021-06-24 14:16:54 +0300

    clear for order

--- app/src/main/java/com/german/androidlearning/MainActivity.java
+++ app/src/main/java/com/german/androidlearning/MainActivity.java
@@ -3,66 +3,38 @@ package com.german.androidlearning;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import com.german.androidlearning.package2.AlignActivity;
-import com.german.androidlearning.package2.ColorActivity;
-import com.german.androidlearning.package2.NameActivity;
-import com.german.androidlearning.package2.ViewActivity;
-//тут то что часто надо
-//implements View.OnClickListener
+import com.german.androidlearning.Package2.ProstoiBrauser;
 
+import static com.german.androidlearning.R.id.button1;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    Button btnWeb;
-    Button btnMap;
-    Button btnCall;
-
-    final int REQUEST_CODE_COLOR = 1;
-    final int REQUEST_CODE_ALIGN = 2;
+    Button button1;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnWeb = (Button) findViewById(R.id.btnWeb);
-        btnMap = (Button) findViewById(R.id.btnMap);
-        btnCall = (Button) findViewById(R.id.btnCall);
+        button1 = findViewById(R.id.button1);
+        button1.setOnClickListener(this);
 
-        btnWeb.setOnClickListener(this);
-        btnMap.setOnClickListener(this);
-        btnCall.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
-            case R.id.btnWeb:
-                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://developer.android.com"));
+            case R.id.button1:
+                Intent intent = new Intent(this, ProstoiBrauser.class);
                 startActivity(intent);
                 break;
-            case R.id.btnMap:
-                intent = new Intent();
-                intent.setAction(Intent.ACTION_VIEW);
-                intent.setData(Uri.parse("geo:55.754283,37.62002"));
-                startActivity(intent);
-                break;
-            case R.id.btnCall:
-                intent = new Intent(Intent.ACTION_DIAL);
-                intent.setData(Uri.parse("tel:12345"));
-                startActivity(intent);
-                break;
+            default:
+            break;
         }
     }
 }
---
+++ app/src/main/java/com/german/androidlearning/Package2/ProstoiBrauser.java
@@ -0,0 +1,17 @@
+package com.german.androidlearning.Package2;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.german.androidlearning.R;
+
+public class ProstoiBrauser extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_prostoi_brauser);
+    }
+}
