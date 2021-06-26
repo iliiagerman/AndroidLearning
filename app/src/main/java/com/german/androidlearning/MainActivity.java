a8a9567e8fa7a9547b38b3bdaabdaac47b155aba
iliia Germaniuk <iliia.germaniuk@gmail.com>
2021-06-26 19:36:40 +0300

    SQLite and all her methods

--- app/src/main/java/com/german/androidlearning/MainActivity.java
+++ app/src/main/java/com/german/androidlearning/MainActivity.java
@@ -9,11 +9,13 @@ import android.widget.Button;
 
 import com.german.androidlearning.Lesons20_40.Preferences;
 import com.german.androidlearning.Lesons20_40.ProstoiBrauser;
+import com.german.androidlearning.Lesons20_40.SQLife;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     Button button1;
     Button button2;
+    Button button3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,8 +24,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         button1 = findViewById(R.id.button1);
         button2 = findViewById(R.id.button2);
+        button3 = findViewById(R.id.button3);
         button1.setOnClickListener(this);
         button2.setOnClickListener(this);
+        button3.setOnClickListener(this);
 
     }
 
@@ -37,6 +41,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.button2:
                 Intent intent1 = new Intent(this, Preferences.class);
                 startActivity(intent1);
+                break;
+            case R.id.button3:
+                Intent intent3 = new Intent(this, SQLife.class);
+                startActivity(intent3);
                 break;
             default:
             break;
