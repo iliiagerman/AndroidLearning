e21231f3efc04d02f5ea5c6efd2060f84b0241b3
iliia Germaniuk <iliia.germaniuk@gmail.com>
2021-07-21 19:24:05 +0300

    no message

--- app/src/main/java/com/german/androidlearning/Lesons20_40/UserInterface/recyclerview/RecyclerViewActivity.java
+++ app/src/main/java/com/german/androidlearning/Lesons20_40/UserInterface/recyclerview/RecyclerViewActivity.java
@@ -16,10 +16,7 @@ public class RecyclerViewActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //
-        //
-        //
-        //т
+        
         setContentView(R.layout.activity_recyclew_view);
 
         numbersList = findViewById(R.id.rv_numbers);
