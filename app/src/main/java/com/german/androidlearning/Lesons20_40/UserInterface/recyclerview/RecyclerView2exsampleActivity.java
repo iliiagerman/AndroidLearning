82cf65ecfab8cd81a238daa5d5f76588b26b991d
iliia Germaniuk <iliia.germaniuk@gmail.com>
2021-07-22 15:06:56 +0300

    no message

---
+++ app/src/main/java/com/german/androidlearning/Lesons20_40/UserInterface/recyclerview/RecyclerView2exsampleActivity.java
@@ -0,0 +1,16 @@
+package com.german.androidlearning.Lesons20_40.UserInterface.recyclerview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.german.androidlearning.R;
+
+public class RecyclerView2exsampleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recycler_view2exsample);
+    }
+}
